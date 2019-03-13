6e9a8444aa9e6e9c2c18412ab7ed35fccafd98d1
Guomingze <44560624+Guomingze@users.noreply.github.com>
2019-03-13 15:31:36 +0800

    2

--- springcloud/springcloud-zuul/src/main/java/com/example/zuul/SpringcloudZuulApplication.java
+++ springcloud/springcloud-zuul/src/main/java/com/example/zuul/SpringcloudZuulApplication.java
@@ -3,6 +3,9 @@ package com.example.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
 @EnableZuulProxy
 @SpringBootApplication
 public class SpringcloudZuulApplication {
