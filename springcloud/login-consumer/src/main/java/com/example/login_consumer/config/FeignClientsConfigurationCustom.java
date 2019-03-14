03675ce2a8a28ac4bd52ab4e7b59281092a185f5
Guomingze <44560624+Guomingze@users.noreply.github.com>
2019-03-14 09:06:29 +0800

    登录

---
+++ springcloud/login-consumer/src/main/java/com/example/login_consumer/config/FeignClientsConfigurationCustom.java
@@ -0,0 +1,42 @@
+package com.example.login_consumer.config;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+
+@Configuration
+public class FeignClientsConfigurationCustom implements RequestInterceptor{
+
+	@Override  
+	  public void apply(RequestTemplate template) {  
+	 
+	    RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();  
+	    if (requestAttributes == null) {  
+	      return;  
+	    }  
+	 
+	    HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();  
+	    Enumeration<String> headerNames = request.getHeaderNames();  
+	    if (headerNames != null) {  
+	      while (headerNames.hasMoreElements()) {  
+	        String name = headerNames.nextElement();  
+	        Enumeration<String> values = request.getHeaders(name);  
+	        while (values.hasMoreElements()) {  
+	          String value = values.nextElement();  
+	          template.header(name, value);  
+	        }  
+	      }  
+	    }  
+	 
+	  }  
+
+}
